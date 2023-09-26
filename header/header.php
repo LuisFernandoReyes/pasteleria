1c6736acbc9821b688e9f6b99baa35c52ebcf942
unknown <65382224+LuisFernandoReyes@users.noreply.github.com>
2023-09-26 12:45:31 -0600

    header DONE

---
+++ header/header.php
@@ -0,0 +1,8 @@
+<nav class="navbar navbar-expand-lg" style="background-color: #E8B0A3;">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="./index.php">Pastelería</a>
+    <a class="navbar-brand" href="./index.php">
+      <img src="./icons/carrito.png" alt="Carrito de compras" style="max-width: 30px; height: auto;">
+    </a>
+  </div>
+</nav>
