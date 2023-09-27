b61de25b62b9f620569a4fdad0462be14e0e6363
unknown <65382224+LuisFernandoReyes@users.noreply.github.com>
2023-09-27 11:25:31 -0600

    changes

--- boxes/boxes.php
+++ boxes/boxes.php
@@ -1,5 +1,5 @@
 <div class="card" style="width: 18rem;">
-  <img src="./img//pastelChocolateFresa.jpg" class="card-img-top">
+  <img src="./img/pastelChocolateFresa.jpg" class="card-img-top">
   <div class="card-body">
     <h5 class="card-title">Pastel de Chocolate</h5>
     <p class="card-text">Este delicioso pastel de chocolate, contiene ricas fresas</p>
